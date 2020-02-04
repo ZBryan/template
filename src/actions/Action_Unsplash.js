a056945d21320d22a3bd503565e37642998e44ac
Zach Bryan <zacharyb@isoftstone.com>
2020-02-04 11:04:17 -0800

    return dispatch

--- src/actions/Action_Unsplash.js
+++ src/actions/Action_Unsplash.js
@@ -1,9 +1,6 @@
 import Unsplash, { toJson } from "unsplash-js";
 export const GET_PHOTO = "GET_PHOTO";
-import fetch from "node-fetch";
-global.fetch = fetch;
-// import fetch from "cross-fetch";
-//move to secret
+
 const api = process.env.REACT_APP_UNSPLASH_API_KEY
 const unsplash = new Unsplash({ accessKey: api });
 export const getPhoto =  () =>  dispatch => {
@@ -12,8 +9,8 @@ export const getPhoto =  () =>  dispatch => {
     .getRandomPhoto({ username: "naoufal" })
     .then(toJson)
     .then(payload => {
-      console.log(payload);
-       dispatch({ type: GET_PHOTO, payload });
+      console.log(`payload`,payload);
+       return dispatch({ type: GET_PHOTO, payload });
     })
     .catch(err => {
       console.log(err);
--- src/reducers/unsplashReducer.js
+++ src/reducers/unsplashReducer.js
@@ -2,6 +2,7 @@ import { GET_PHOTO } from "../actions/Action_Unsplash";
 
 export default function(state = null, action) {
   console.log(action.type)
+  console.log(GET_PHOTO)
   switch (action.type) {
     case GET_PHOTO:
       console.log(`ran reducer`, action.payload);
